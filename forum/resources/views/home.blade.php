1572430cb49b124f514e03a173da51cf44e9bfb5
Omar Larhnimi <larhnimikit@gmail.com>
2020-05-15 16:22:45 +0000

    ...

---
+++ forum/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+ @section('content')
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum eveniet laborum, ex inventore cumque sint ipsam, nobis, iusto perferendis doloremque consequuntur. Ex tenetur expedita deserunt provident quidem animi cupiditate eum!</p>
+@endsection
+    
---
+++ forum/routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+// prend deux parameters
+/**
+ * 1 : la route
+ * 2 : le nom de la vue qui sera afficher
+ */
+Route::view('/', 'home');
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+// creation d'une routes avec segement dynamic + function anonyme
+Route::get('/post/{id}', function($id){
+    return $id;
+});
+
+// creation d'une routes avec segement dynamic + function anonyme
+// qui faire passer des donnes a une vue
+# on peut rendre une parameter dans la route optional via l'utilisation
+## du ? exp : `{author?}` et on lui donne une valeur par default
+### dans la function anonyme
+Route::get('/posts/{id}/{author?}', function($myId, $myAuthor = 'default'){
+
+    // les donnes qu'on veut les transmetre vers la view `show.blade.php`
+    $posts = [
+        1 => ['title' => 'learn data'],
+        2 => ['title' => 'learn some thing']
+    ];
+    // on a posts qui est un dossier, et show le fichier
+    return view('posts.show', [
+        // dans la procedure de recuperation on va utilise le nom `data`
+        // on utilise la valeur id passer on parameters, pour 
+        // recuperer seulement les information demander par le lien
+        'data' => $posts[$myId],
+        'author' => $myAuthor
+    ]);
+});
